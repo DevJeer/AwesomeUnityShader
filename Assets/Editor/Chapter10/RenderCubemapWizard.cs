dc92c26ecd97cfea3457c6ea1ef85f32788759aa
houjf <18829536923@163.com>
2021-04-27 17:56:37 +0800

    实现环境映射 1. 实现反射

---
+++ Assets/Editor/Chapter10/RenderCubemapWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RenderCubemapWizard : ScriptableWizard{
+
+    public Transform renderFromPosition;
+    public Cubemap cubemap;
+
+    void OnWizardUpdate()
+    {
+        helpString = "Selcet transform to render from and cubemap to render into...";
+        isValid = (renderFromPosition != null) && (cubemap != null);
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubemapCamera");
+        go.AddComponent<Camera>();
+        go.transform.position = renderFromPosition.position;
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("GameObject/渲染cubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard<RenderCubemapWizard>("渲染cubemap", "渲染!!!");
+    }
+
+
+
+}
